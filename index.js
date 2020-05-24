88ad0087f2ea33f5a71282a1039aa3c8b158a510
Vi <druzica34@seznam.cz>
2020-05-24 21:42:20 +0200

    oprava ukolu

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 'use strict';
 
-const taskElm = document.querySelector(".todo__tasks");
-
 const tasks = [
-  'Když vstaneš, choď se umýt',
+  'Když vstaneš, prosím choď se umýt',
   'Pak si dej snídani',
   'V 10h ti začína Online výuka, připrav si předtím věci na hodinu',
   'Oběd budeme mít spolu, prosím počkej na mně'
 ];
 
 const updateTasks = () => {
-  taskElm.innerHTML = "";
-  const newTaskElm = document.querySelector(".new-task");
-  tasks.push(newTaskElm.value);
-
+  const listElm = document.querySelector(".todo__tasks");
+  listElm.innerHTML = '';
   for (let i = 0; i < tasks.length; i += 1) {
-    taskElm.innerHTML += `<div class="task">${tasks[i]}</div>`;
+    listElm.innerHTML += `<div class="task">${tasks[i]}</div>`;
   }
 };
-updateTasks();
-
-const btnElm = document.querySelector('button');
-btnElm.addEventListener('click', updateTasks);
+updateTasks()
+const addTask = () => {
+  const newTaskElm = document.querySelector(".new-task");
+  tasks.push(newTaskElm.value);
+  updateTasks();
+  newTaskElm.value = '';
+}
+const btnElm = document.querySelector("button");
+btnElm.addEventListener("click", addTask);
 
 
 
